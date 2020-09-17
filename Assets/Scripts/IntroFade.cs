f178a4f91fe17fcfddf4b91bd1957fd0119d76c0
aidan-lee123 <aidan.lee123@gmail.com>
2020-09-17 16:15:06 +1000

    Added in Audio Files

---
+++ Assets/Scripts/IntroFade.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntroFade : MonoBehaviour
+{
+
+    public AudioClip introSong;
+    public AudioClip ghostNormal;
+
+    public AudioSource source;
+
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+
+        StartCoroutine(PlayIntro());
+    }
+
+    IEnumerator PlayIntro() {
+        source.clip = introSong;
+        source.Play();
+        yield return new WaitForSeconds(introSong.length);
+        source.clip = ghostNormal;
+        source.Play();
+        source.loop = true;
+    }
+}
