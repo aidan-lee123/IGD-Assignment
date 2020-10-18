77d2eba5a3d2703db86026ab8ffa7fbbd575a4be
aidan-lee123 <aidan.lee123@gmail.com>
2020-10-18 21:17:14 +1100

    Added Teleporter and Sound System
    Sound System works but doesn't play things consistently because its constantly setting the clip to be another clip not sure how to change that?

--- Assets/Scripts/Player/PacStudentController.cs
+++ Assets/Scripts/Player/PacStudentController.cs
@@ -12,6 +12,10 @@ public class PacStudentController : MonoBehaviour
     private KeyCode currentInput;
     private bool isMoving = false;
 
+    public AudioClip pacStudentMove;
+    public AudioClip pacStudentPellet;
+    public AudioSource audioSource;
+
     //The lower the faster
     public float speed = 1f;
     // Start is called before the first frame update
@@ -19,7 +23,8 @@ public class PacStudentController : MonoBehaviour
     {
         tweener = GetComponent<Tweener>();
         map = GameObject.Find("Level Generator").GetComponent<LevelGenerator>().levelMap;
-        CheckDirection(KeyCode.None);
+        audioSource = GetComponent<AudioSource>();
+
         this.transform.position = new Vector2(1, -1);
     }
 
@@ -71,27 +76,27 @@ public class PacStudentController : MonoBehaviour
 
         switch (direction) {
             case KeyCode.W:
-                if (CheckDirection(KeyCode.W)) {
-                    this.transform.rotation = Quaternion.Euler(0, 0, 90);
+                if (CheckSound(KeyCode.W)) {
                     tweener.AddTween(this.transform, this.transform.position, new Vector3(this.transform.position.x, this.transform.position.y + 1f, 0f), speed);
+                    this.transform.rotation = Quaternion.Euler(0, 0, 90);
                 }
                 break;
             case KeyCode.A:
-                if (CheckDirection(KeyCode.A)) {
-                    this.transform.rotation = Quaternion.Euler(0, 0, 180);
+                if (CheckSound(KeyCode.A)) {
                     tweener.AddTween(this.transform, this.transform.position, new Vector3(this.transform.position.x - 1f, this.transform.position.y, 0f), speed);
+                    this.transform.rotation = Quaternion.Euler(0, 180, 0);
                 }
                 break;
             case KeyCode.S:
-                if (CheckDirection(KeyCode.S)) {
-                    this.transform.rotation = Quaternion.Euler(0, 0, -90);
+                if (CheckSound(KeyCode.S)) {
                     tweener.AddTween(this.transform, this.transform.position, new Vector3(this.transform.position.x, this.transform.position.y - 1f, 0f), speed);
+                    this.transform.rotation = Quaternion.Euler(0, 0, -90);
                 }
                 break;
             case KeyCode.D:
-                if (CheckDirection(KeyCode.D)) {
-                    this.transform.rotation = Quaternion.Euler(0, 0, 0);
+                if (CheckSound(KeyCode.D)) {
                     tweener.AddTween(this.transform, this.transform.position, new Vector3(this.transform.position.x + 1f, this.transform.position.y, 0f), speed);
+                    this.transform.rotation = Quaternion.Euler(0, 0, 0);
                 }
                 break;
         }
@@ -165,6 +170,109 @@ public class PacStudentController : MonoBehaviour
                     currentInput = direction;
                 }
                 break;
+            default:
+                break;
+        }
+        return clear;
+    }
+
+
+    //this is the same as checkdirecion except this one also sets the sound becasue
+    //i wasn't sure how to do this otherwise besides with a raycast but I thought
+    // That that would have some strange effects
+    public bool CheckSound(KeyCode direction) {
+        bool clear = false;
+
+        int[,] surroundingMap = new int[3, 3];
+        int xCount = -1;
+        int yCount = -1;
+
+        string array = "";
+
+        int xPos = (int)this.transform.position.x;
+        int yPos = (int)this.transform.position.y;
+        for (int x = 0; x < 3; x++) {
+
+            for (int y = 0; y < 3; y++) {
+
+                try {
+                    //Gotta * by -1 otherwise map is upside down
+                    //I also messed up the way arrays work ages ago so x and y are swapped in every scenario
+                    //meaning i needed to swap it here to get it to work and that took me actually ages to figure out
+                    surroundingMap[x, y] = map[Mathf.FloorToInt(-yPos) + xCount, Mathf.FloorToInt(xPos) + yCount];
+                }
+                catch {
+                    surroundingMap[x, y] = 0;
+                }
+                array += "{" + surroundingMap[x, y] + "}, ";
+                yCount++;
+            }
+            yCount = -1;
+            array += "\n";
+            xCount++;
+        }
+
+
+        /*
+        00 01 02
+        10 11 12
+        20 21 22
+        */
+
+        switch (direction) {
+            case KeyCode.W: {
+                    if (surroundingMap[0, 1] == 5 || surroundingMap[0, 1] == 6) {
+                        clear = true;
+                        audioSource.clip = pacStudentPellet;
+                        currentInput = direction;
+                    }
+
+                    else if (surroundingMap[0, 1] == 0) {
+                        clear = true;
+                        audioSource.clip = pacStudentMove;
+                        currentInput = direction;
+                    }
+                    break;
+                }
+            case KeyCode.A: {
+                    if (surroundingMap[1, 0] == 5 || surroundingMap[1, 0] == 6) {
+                        clear = true;
+                        audioSource.clip = pacStudentPellet;
+                        currentInput = direction;
+                    }
+                    if (surroundingMap[1, 0] == 0) {
+                        clear = true;
+                        audioSource.clip = pacStudentMove;
+                        currentInput = direction;
+                    }
+                    break;
+                }
+            case KeyCode.S: {
+                    if (surroundingMap[2, 1] == 5 || surroundingMap[2, 1] == 6) {
+                        clear = true;
+                        audioSource.clip = pacStudentPellet;
+                        currentInput = direction;
+                    }
+                    if (surroundingMap[2, 1] == 0) {
+                        clear = true;
+                        audioSource.clip = pacStudentMove;
+                        currentInput = direction;
+                    }
+                    break;
+                }
+            case KeyCode.D: {
+                    if (surroundingMap[1, 2] == 5 || surroundingMap[1, 2] == 6) {
+                        clear = true;
+                        audioSource.clip = pacStudentPellet;
+                        currentInput = direction;
+                    }
+                    if (surroundingMap[1, 2] == 0) {
+                        clear = true;
+                        audioSource.clip = pacStudentMove;
+                        currentInput = direction;
+                    }
+                    break;
+                }
             default:
                 break;
         }
